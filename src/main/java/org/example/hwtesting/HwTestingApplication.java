3d8aee46757df80a1b63e501598ba2160b4dca4f
Лаисат <lz_256@mail.ru>
2024-05-01 15:09:04 +0300

    Comment

---
+++ src/main/java/org/example/hwtesting/HwTestingApplication.java
@@ -0,0 +1,13 @@
+package org.example.hwtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwTestingApplication.class, args);
+    }
+
+}
